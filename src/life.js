65af2d3f1a52c6568402a590713d6e9e0bf3109f
Екатерина <polskaya007@mail.ru>
2023-04-03 22:06:26 +0300

    version 1

---
+++ src/life.js
@@ -0,0 +1,12 @@
+
+export default function showLifeBar(user) {
+	if (user.health >= 50) {
+		return 'healthy';
+	}
+
+	if (user.health >= 15 && user.health < 50) {
+		return 'wounded';
+	}
+
+	return 'critical';
+}
